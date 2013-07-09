3013098c9a7b0a9d6902eb97387ecdecd2d71742
Ross Lewis <rosswlewis@Rosss-MacBook-Pro.local>
2013-07-08 18:14:02 -0700

    7/8/2013
    just put on the app store as this version is!

---
+++ CelebPackPurchases.h
@@ -0,0 +1,13 @@
+//
+//  CelebPackPurchases.h
+//  Search Party
+//
+//  Created by Ross Lewis on 5/16/13.
+//  Copyright (c) 2013 Ross Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CelebPackPurchases : UIAlertView
+
+@end
---
+++ CelebPackPurchases.m
@@ -0,0 +1,31 @@
+//
+//  CelebPackPurchases.m
+//  Search Party
+//
+//  Created by Ross Lewis on 5/16/13.
+//  Copyright (c) 2013 Ross Lewis. All rights reserved.
+//
+
+#import "CelebPackPurchases.h"
+
+@implementation CelebPackPurchases
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
